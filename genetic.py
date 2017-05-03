673bb7909bab5e087a844da1e6642e3f5a753efa
Sampanna-Sharma <073bct538,sampanna@pcampus.edu.np>
2017-05-03 23:48:09 +0545

    main file

--- genetic.py
+++ genetic.py
@@ -1,8 +1,20 @@
 import os
 import random
+from PIL import Image
+from PIL import ImageDraw
 
 NO_OF_SPECIES = 100
 NO_OF_GENE = 100
+
+
+#Checking if the image file exists or not
+try :
+    globalTarget = Image.open("reference.png")
+except IOError:
+    print("There is no reference.png file in the directory")
+    exit()
+
+
 
 
 
@@ -21,6 +33,26 @@ class Gene:
     def __init__(self, size):
         self.size = size
         self.diameter = random.randint(2, 15)
-        self.pos = Point(random.randint(0, size[0]), random.randint(0, size[1]))
+        self.pos = Point(random.randint(0, self.size[0]), random.randint(0, self.size[1]))
         self.color = Color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        
+        self.paras = {'diameter', 'position', 'color'}
+
+    def mutate(self):
+        mutate_type = random.choice(self.paras)
+
+        if mutate_type == 'diameter':
+            self.diameter = random.randint(2, 15)
+        elif mutate_type == 'position':
+            self.pos = Point(random.randint(0, self.size[0]), random.randint(0, self.size[1]))
+        elif mutate_type == 'color':
+            self.color = Color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+class Organism:
+    def __index__(self, size):
+        self.size = size
+        self.organism = [Gene(self.size) for _ in range(NO_OF_GENE)]
+
+
+
+
+
