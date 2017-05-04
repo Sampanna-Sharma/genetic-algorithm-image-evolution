0af2fec3ec8df09a1a2be6e4c434a18e5a02cd0c
Sampanna-Sharma <073bct538,sampanna@pcampus.edu.np>
2017-05-04 23:40:31 +0545

    almost done

--- genetic.py
+++ genetic.py
@@ -4,6 +4,8 @@ from PIL import Image
 from PIL import ImageDraw
 import numpy
 import multiprocessing
+from copy import deepcopy
+
 
 NO_OF_SPECIES = 100
 NO_OF_GENE = 100
@@ -38,7 +40,7 @@ class Gene:
         self.diameter = random.randint(2, 15)
         self.pos = Point(random.randint(0, self.size[0]), random.randint(0, self.size[1]))
         self.color = Color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        self.paras = {'diameter', 'position', 'color'}
+        self.paras = ['diameter', 'position', 'color']
 
     def mutate(self):
         mutate_type = random.choice(self.paras)
@@ -52,21 +54,22 @@ class Gene:
 
 
 class Organism:
-    def __index__(self, size):
+    def __init__(self, size):
         self.size = size
         self.organism = [Gene(self.size) for _ in range(NO_OF_GENE)]
 
     def mutate(self):
         for g in self.organism:
-            if MUTATION_CHANCE > random.random:
+            if MUTATION_CHANCE > random.random():
                 g.mutate()
 
     def draw_image(self):
         image = Image.new("RGB", self.size, (255, 255, 255))
-        canvas = ImageDraw.Draw(image, 'RGBA')
+        canvas = ImageDraw.Draw(image, 'RGB')
         for g in self.organism:
-            color = (g.color.r, g.color.g, g.color.b, random.random)
-            canvas.ellipse([g.pos.x-g.diameter, g.pos.y-g.diameter ,g.pos.x+g.diameter, g.pos.y+g.diameter], outline = color, fill = color)
+            color = (g.color.r, g.color.g, g.color.b)
+            canvas.ellipse([g.pos.x - g.diameter, g.pos.y - g.diameter, g.pos.x + g.diameter, g.pos.y + g.diameter],
+                           outline=color, fill=color)
             return image
 
 
@@ -74,7 +77,7 @@ def calcfitness(i1, i2):
     p1 = numpy.array(i1, numpy.int16)
     p2 = numpy.array(i2, numpy.int16)
     dif = numpy.sum(numpy.abs(p1-p2))
-    fitness = 100 - ((dif/255.0)*100)/p1.size
+    fitness = ((dif/255.0)*100)/p1.size
     return fitness
 
 
@@ -82,7 +85,7 @@ def run():
     if not os.path.exists('results'):
         os.mkdir('results')
 
-    fp = open(os.path.join('results','log.txt'), 'a')
+    fp = open(os.path.join('results', 'log.txt'), 'a')
     target = globalTarget
 
     generation = 1
@@ -90,7 +93,7 @@ def run():
     parent = Organism(target.size)
     score = calcfitness(parent.draw_image(), target)
 
-    p = multiprocessing.Pool()
+    p = multiprocessing.Pool(2)
 
     while True:
         print("Genreation {}- {}".format(generation, score))
@@ -100,17 +103,43 @@ def run():
         generation += 1
         children = []
         scores = []
-        result = groupmutate(parent, NO_OF_SPECIES-1, p)
+        children.append(parent)
+        scores.append(score)
+        try:
+            result = groupmutate(parent, NO_OF_SPECIES-1, p)
+        except KeyboardInterrupt:
+            print("bye")
+            p.close()
+            return
+        newscore, newchild = zip(*result)
+        children.extend(newchild)
+        scores.extend(newscore)
+        winner = sorted(zip(children, scores), key=lambda x: x[1])
+        parent, score = winner[0]
 
 
 
 
+def mutateandgo(o):
+    try:
+        c = deepcopy(o)
+        c.mutate()
+        i1 = c.draw_image()
+        i2 = globalTarget
+        return calcfitness(i1, i2, ), c
+    except KeyboardInterrupt:
+        pass
 
 
-
-def groupmutate(o, n, p):
-    results = p.map(mutateandgo, o*int(n)
+def groupmutate(o, number, p):
+    results = p.map(mutateandgo, [o]*int(number))
+    #results = [mutateandgo(i) for i in [o]*int(number)]
     return results
+
+if __name__ == '__main__':
+    run()
+
+
 
 
 
